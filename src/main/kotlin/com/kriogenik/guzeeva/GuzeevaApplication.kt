f4c4a7c24dd72bf99cd17bbad26af81be1ee9a6c
Kriogenik <thekriogenik@gmail.com>
2020-01-18 00:53:08 +0300

    Initial commit

---
+++ src/main/kotlin/com/kriogenik/guzeeva/GuzeevaApplication.kt
@@ -0,0 +1,11 @@
+package com.kriogenik.guzeeva
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GuzeevaApplication
+
+fun main(args: Array<String>) {
+	runApplication<GuzeevaApplication>(*args)
+}
---
+++ src/test/kotlin/com/kriogenik/guzeeva/GuzeevaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kriogenik.guzeeva
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GuzeevaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

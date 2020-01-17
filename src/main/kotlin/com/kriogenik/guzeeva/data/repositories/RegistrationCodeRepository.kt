03062b60281a002b8dd1c3dfc388f9ba0f8b095d
Kriogenik <thekriogenik@gmail.com>
2020-01-18 02:03:57 +0300

    Added findByCode function

--- src/main/kotlin/com/kriogenik/guzeeva/data/repositories/RegistrationCodeRepository.kt
+++ src/main/kotlin/com/kriogenik/guzeeva/data/repositories/RegistrationCodeRepository.kt
@@ -3,6 +3,11 @@ package com.kriogenik.guzeeva.data.repositories
 import com.kriogenik.guzeeva.model.RegistrationCode
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
-interface RegistrationCodeRepository: CrudRepository<RegistrationCode, Int>
+interface RegistrationCodeRepository: CrudRepository<RegistrationCode, Int>{
+
+    fun findByCode(code: String): Optional<RegistrationCode>
+
+}

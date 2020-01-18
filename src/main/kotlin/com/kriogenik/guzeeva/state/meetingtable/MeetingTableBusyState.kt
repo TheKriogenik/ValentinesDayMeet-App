d68ea5aaff052bfc7cb3a8b14277d05c01d4f835
Kriogenik <thekriogenik@gmail.com>
2020-01-18 03:54:47 +0300

    Added MeetingTableBusyState

---
+++ src/main/kotlin/com/kriogenik/guzeeva/state/meetingtable/MeetingTableBusyState.kt
@@ -0,0 +1,31 @@
+package com.kriogenik.guzeeva.state.meetingtable
+
+import com.kriogenik.guzeeva.data.services.MeetingTableService
+import com.kriogenik.guzeeva.model.MeetingTable
+import com.kriogenik.guzeeva.model.TableEntityState
+import com.kriogenik.guzeeva.state.State
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class MeetingTableBusyState: State<MeetingTable> {
+
+    @Autowired
+    private lateinit var service: MeetingTableService
+
+    private final val log = LoggerFactory.getLogger(this::class.java)
+
+    override fun execute(context: MeetingTable): Optional<MeetingTable> {
+        log.debug("Changing state of MeetingTable#${context.id}.")
+        return context.copy(state = TableEntityState.BUSY).let(service::update).map {
+            log.debug("State of MeetingTable#${context.id} changed.")
+            Optional.of(it)
+        }.orElseGet{
+            log.error("State of MeetingTable#${context.id} not changed!")
+            Optional.empty<MeetingTable>()
+        }
+    }
+
+}
---
+++ src/test/kotlin/com/kriogenik/guzeeva/state/meetingtable/MeetingTableBusyStateTest.kt
@@ -0,0 +1,41 @@
+package com.kriogenik.guzeeva.state.meetingtable
+
+import com.kriogenik.guzeeva.data.services.MeetingTableService
+import com.kriogenik.guzeeva.model.MeetingTable
+import com.kriogenik.guzeeva.model.TableEntityState
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import java.util.*
+
+@SpringBootTest
+class MeetingTableBusyStateTest {
+
+    @Autowired
+    private lateinit var meetingTableBusyState: MeetingTableBusyState
+
+    @MockBean
+    private lateinit var meetingTableService: MeetingTableService
+
+    @Test
+    fun changeState(){
+        val initialTable = MeetingTable(0, state = TableEntityState.FREE)
+        val resultTable  = MeetingTable(0, state = TableEntityState.BUSY)
+        Mockito.`when`(meetingTableService.find(initialTable.id))
+                .thenReturn(Optional.of(initialTable))
+        Mockito.`when`(meetingTableService.update(resultTable))
+                .thenReturn(Optional.of(resultTable))
+        assert(meetingTableBusyState.execute(initialTable) == Optional.of(resultTable))
+    }
+
+    @Test
+    fun changeStateError(){
+        val initialTable = MeetingTable(0, state = TableEntityState.FREE)
+        Mockito.`when`(meetingTableService.find(initialTable.id))
+                .thenReturn(Optional.empty())
+        assert(meetingTableBusyState.execute(initialTable) == Optional.empty<MeetingTable>())
+    }
+
+}

e948aa5fde60cb65428c4147bc893eae286b4f5a
Kriogenik <thekriogenik@gmail.com>
2020-01-19 14:53:27 +0300

    Added empty constructor for Keyboard

--- src/main/kotlin/com/kriogenik/guzeeva/messaging/model/Keyboard.kt
+++ src/main/kotlin/com/kriogenik/guzeeva/messaging/model/Keyboard.kt
@@ -1,5 +1,11 @@
 package com.kriogenik.guzeeva.messaging.model
 
+import java.util.*
+
 data class Keyboard(
         val rows: List<List<Key>>
-)
+){
+    companion object{
+        fun empty() = Keyboard(Collections.emptyList())
+    }
+}
